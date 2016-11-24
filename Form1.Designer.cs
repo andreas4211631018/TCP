6569b1250d980a08d4c5bda61976acabd99a2f01
andreas4211631018 <andreas.supriyanta@gmail.com>
2016-11-24 23:44:51 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonLedOn = new System.Windows.Forms.Button();
+            this.buttonLedOff = new System.Windows.Forms.Button();
+            this.buttonCnnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(76, 16);
+            this.textBoxIP.Multiline = true;
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIP.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(76, 42);
+            this.textBoxPort.Multiline = true;
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port :";
+            // 
+            // buttonLedOn
+            // 
+            this.buttonLedOn.Location = new System.Drawing.Point(21, 80);
+            this.buttonLedOn.Name = "buttonLedOn";
+            this.buttonLedOn.Size = new System.Drawing.Size(251, 23);
+            this.buttonLedOn.TabIndex = 4;
+            this.buttonLedOn.Text = "LED On";
+            this.buttonLedOn.UseVisualStyleBackColor = true;
+            this.buttonLedOn.Click += new System.EventHandler(this.buttonLedOn_Click);
+            // 
+            // buttonLedOff
+            // 
+            this.buttonLedOff.Location = new System.Drawing.Point(21, 125);
+            this.buttonLedOff.Name = "buttonLedOff";
+            this.buttonLedOff.Size = new System.Drawing.Size(251, 23);
+            this.buttonLedOff.TabIndex = 5;
+            this.buttonLedOff.Text = "LED Off";
+            this.buttonLedOff.UseVisualStyleBackColor = true;
+            this.buttonLedOff.Click += new System.EventHandler(this.buttonLedOff_Click);
+            // 
+            // buttonCnnect
+            // 
+            this.buttonCnnect.Location = new System.Drawing.Point(197, 14);
+            this.buttonCnnect.Name = "buttonCnnect";
+            this.buttonCnnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonCnnect.TabIndex = 6;
+            this.buttonCnnect.Text = "Connect";
+            this.buttonCnnect.UseVisualStyleBackColor = true;
+            this.buttonCnnect.Click += new System.EventHandler(this.buttonCnnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.Controls.Add(this.buttonCnnect);
+            this.Controls.Add(this.buttonLedOff);
+            this.Controls.Add(this.buttonLedOn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonLedOn;
+        private System.Windows.Forms.Button buttonLedOff;
+        private System.Windows.Forms.Button buttonCnnect;
+    }
+}
+
